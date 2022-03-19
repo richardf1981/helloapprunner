d79d5a4c49b0fdd3f2f05ac3af25736afbfb4552
richardf1981 <66539291+richardf1981@users.noreply.github.com>
2022-03-19 20:20:49 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print("hello!!!")
     return 'Hello AWS App Runner'
 
-app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    print("hello!!!")
+    app.run(host='0.0.0.0', port=80)
